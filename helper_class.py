d369b554130a88739b647f77b162a518d76519a4
Zaahir Dawood <zaahirdawood@gmail.com>
2023-04-26 19:20:35 +0000

    Main create a helper class function to interact with googlesheets using the python API & google colab

---
+++ helper_class.py
@@ -0,0 +1,56 @@
+# A helper class & its functions to interact with Google Sheets using Python used in google colab
+
+from google.colab import auth
+import gspread
+from google.auth import default
+import pandas as pd
+
+class GoogleSheets:
+
+  def __init__(self,sheet_url,sheet_name,work_sheet):
+    
+    self.split_links = sheet_url.split('/')
+    self.sheet_key = self.split_links[5]
+    self.ss_name = sheet_name
+    self.ws = work_sheet 
+
+  def __str__(self):
+    return "class instantiated for {} workbook and {} worksheet.".format(self.sheet_name,self.ws)
+
+  def returnClientObject(self):
+    auth.authenticate_user()
+    creds, _ = default()
+    return creds 
+
+  def create_sheet(self,object_cred):
+    self.object_cred = object_cred
+    gc = gspread.authorize(self.object_cred)
+    sh = gc.create(self.ss_name)
+
+  def pull_sheet_todf(self,creds):
+    self.creds = creds
+    gc = gspread.authorize(self.creds)
+    wb = gc.open_by_key(self.sheet_key)
+    ws = wb.worksheet(self.ws)
+    rows = ws.get_all_values()
+    df = pd.DataFrame.from_records(rows[1:],columns=rows[0])
+    return df
+
+  def update_ghseets_fromdf(self,creds,data):
+    self.worksheet = work_sheet
+    self.creds = creds
+    gc = gspread.authorize(self.creds)
+    wb = gc.open_by_key(self.sheet_key)
+    ws = wb.worksheet(self.ws)
+    ws.update([data.columns.values.tolist()] + data.values.tolist() )
+    return "{} has been updated with new data".format(self.ws)
+  
+  def clear_gsheets(self,creds):
+    self.worksheet = work_sheet
+    self.creds = creds
+    gc = gspread.authorize(self.creds)
+    wb = gc.open_by_key(self.sheet_key)
+    ws = wb.worksheet(self.ws)
+    ws.clear()
+    return "{} has been wiped".format(self.ws)
+
